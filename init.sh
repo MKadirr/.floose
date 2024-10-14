4f7bc9d3c27657f88a7f1615f06c90e64f6ab7ae
Simon Lecubin <simon.lecubin@epita.fr>
2024-10-14 19:30:51 +0200

    updating structure and init

--- init.sh
+++ init.sh
@@ -1,29 +1,61 @@
 #!/bin/sh
 
-backup="$HOME/afs/.confs/.bckupf/"
+configf="$HOME/afs/.confs"
+backup="$configf/.bckupf"
 lock="$HOME/afs/.lock"
 
-replace(){
+target="bashrc config/alacritty config/i3"
 
-    if [ ! -d "$backup" ]; then
-        echo "Backup not found. Creating one"
-    fi
+replace(){
+    echo " - Replacing $1"
+    printf "%s" "   Saving: "
+    cp -r $configf/$1 $backup/$1
+    echo "Done"
+    printf "%s" "   Replacing: "
+    echo -- "cp -r ./$1 $configf/$1"
+    echo "Done"
+}
+
+reverse(){
+    echo " - Restauring $1"
+    printf "%s" "   Restaure: "
+    cp -r $backup/$1 $configf/$1
+    echo "Done"
 }
 
 init(){
-    echo "init"
+    echo "Init:"
+
+    if [ ! -d $backup ]; then
+        echo " - Creating backup folder"
+        mkdir $backup
+        mkdir $backup/config
+    fi
+
+    for d in $target; do
+        replace $d
+    done
 
     touch $lock
 }
 
-reverse(){
-    echo "reverse"
+unfloose(){
+    echo "Reverse:"
 
-    rm $lock
+    if [ ! -d $backup ]; then
+        echo "Backup folder not found. Exiting"
+        rm $lock
+        return
+    fi
+
+    reverse bashrc
+
+    echo "Remove $lock to restart it"
 }
 
+
 if [ -f $lock ]; then
-    reverse
+    unfloose
 else
     init
 fi
